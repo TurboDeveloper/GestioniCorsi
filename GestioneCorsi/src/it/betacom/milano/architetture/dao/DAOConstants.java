f07f6118ec050b7916436b22b79e051dd9b60c7f
kingo <kingo@DESKTOP-P0DINSI.lan>
2020-12-21 17:18:02 +0100

    SEQUENCE DAOConstants

---
+++ GestioneCorsi/src/it/betacom/milano/architetture/dao/DAOConstants.java
@@ -0,0 +1,12 @@
+package it.betacom.milano.architetture.dao;
+
+public interface DAOConstants {
+	String AMMINISTRATORE_SEQUENCE="select amministratore_sequence.nextval from dual";
+	String DOCENTE_SEQUENCE="select docente_sequence.nextval from dual";
+	String CORSISTA_SEQUENCE="select corsista_sequence.nextval from dual";
+	String CORSI_SEQUENCE="select corsi_sequence.nextval from dual";
+	
+
+}
+
+
