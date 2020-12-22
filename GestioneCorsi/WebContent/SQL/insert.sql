c14388b4b7c3743b45d4aac4f589dfbc066ce130
kingo <kingo@DESKTOP-P0DINSI.lan>
2020-12-22 15:54:30 +0100

    Inserite insert Amministratori e docenti

---
+++ GestioneCorsi/WebContent/SQL/insert.sql
@@ -0,0 +1,8 @@
+insert into AMMINISTRATORE values(amministratore_sequence.nextval, 'Massimo','Rossi','pass' );
+insert into AMMINISTRATORE values(amministratore_sequence.nextval, 'Gennaro','Esposito', 'pass');
+
+insert into DATI_DOCENTI values(docente_sequence.nextval,'Francesco','Fumagalli','CvFumagalli.txt');
+insert into DATI_DOCENTI values(docente_sequence.nextval, 'Ambrogio', 'Brambilla', 'CvBrambilla.txt');
+insert into DATI_DOCENTI values(docente_sequence.nextval, 'Giuseppe', 'Verdi', 'CvVerdi.txt');
+insert into DATI_DOCENTI values(docente_sequence.nextval, 'Armando', 'Diaz', 'CvDiaz.txt');
+insert into DATI_DOCENTI values(docente_sequence.nextval, 'Antonio', 'Lo Cascio', 'CvLoCascio.txt');
