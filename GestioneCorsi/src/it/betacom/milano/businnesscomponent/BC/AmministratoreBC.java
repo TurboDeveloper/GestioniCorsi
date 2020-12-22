3ed143466ea4a2e44052be2f23e557980165704f
kingo <kingo@DESKTOP-P0DINSI.lan>
2020-12-22 17:29:34 +0100

    Implementato AmministratoreBC

---
+++ GestioneCorsi/src/it/betacom/milano/businnesscomponent/BC/AmministratoreBC.java
@@ -0,0 +1,25 @@
+package it.betacom.milano.businnesscomponent.BC;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import it.betacom.milano.architetture.dao.AmministratoreDAO;
+import it.betacom.milano.architetture.dao.DAOException;
+import it.betacom.milano.architetture.dao.DBAccess;
+import it.betacom.milano.businesscomponent.model.Amministratore;
+
+public class AmministratoreBC {
+private Connection conn;
+	
+	public AmministratoreBC() throws DAOException, ClassNotFoundException, IOException {
+		conn = DBAccess.getConnection();
+	}
+	public Amministratore getById(long id) throws DAOException {
+		try {
+			return AmministratoreDAO.getFactory().getById(conn, id);
+		}catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+}
