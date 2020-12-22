16b9413a2f20409ceb36fbcd4eb0f501633da571
kingo <kingo@DESKTOP-P0DINSI.lan>
2020-12-22 11:19:52 +0100

    select durata corso?

--- GestioneCorsi/src/it/betacom/milano/architetture/dao/DAOConstants.java
+++ GestioneCorsi/src/it/betacom/milano/architetture/dao/DAOConstants.java
@@ -10,7 +10,13 @@ public interface DAOConstants {
 	String SELECT_COUNTCOMMENTI="Select count(commenti_corso) from dati_corsi";
 /*???*/	String SELECT_CORSOXNMAGGCORSISTI="select cod_corso, count(cod_corsista) from corso_corsista group by cod_corso fetch 1 rows";
 	String SELECT_DATAINIZIOULTIMOCORSO=" Select nome_corso,data_iniziocorso from dati_corsi where data_iniziocorso=(select MAX(data_iniziocorso) from dati_corsi)";
-	String SELECT_AVGDURATACORSI="select nome_corso, avg(data_finecorso - data_iniziocorso) from dati_corsi group by nome_corso not in ( 'SAT', 'SUN' )";
+	String SELECT_AVGDURATACORSI="select nome_corso, data_finecorso, data_iniziocorso SELECT OrderNumber, InstallDate, CompleteDate,\r\n"
+			+ "  (TRUNC(data_finecorso) - TRUNC(data_iniziocorso) ) +1 - \r\n"
+			+ "  ((((TRUNC(data_finecorso,'D'))-(TRUNC(data_iniziocorso,'D')))/7)*2) -\r\n"
+			+ "  (CASE WHEN TO_CHAR(InstallDate,'DY','nls_date_language=english')='SUN' THEN 1 ELSE 0 END) -\r\n"
+			+ "  (CASE WHEN TO_CHAR(CompleteDate,'DY','nls_date_language=english')='SAT' THEN 1 ELSE 0 END) as BusinessDays\r\n"
+			+ "FROM Orders\r\n"
+			+ "ORDER BY OrderNumber;";
 	String SELECT_CORSISTI="Select * from dati_corsisti ";
 	String SELECT_CORSIDISPONIBILI="Select * from dati_corsi where posti_disponibili>0 oder by posti_disponibili"; //(posti_totali-prenotati)as posti_disponibili>0
 	String DELETE_CORSI="Delete from dati_corsi where cod_corso=?";
@@ -19,8 +25,6 @@ public interface DAOConstants {
 	String UPDATE_AMMINISTRATORE="Update amministratore set nome_admin=? cognome_admin=? password_admin where cod_amin=? ";
 	String DELETE_AMMINISTRATORE="Delete from amministratore where cod_admin=?";
 	String SELECT_AMMINISTRATORE_BYID="Select * from amministratore where cod_admin=?";
-	
-	String SELECT_CORSISTASEQ = "Select corsista_sequence.nextval from dual";
 }
 
 
