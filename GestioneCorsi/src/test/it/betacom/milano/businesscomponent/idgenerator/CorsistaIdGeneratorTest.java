9252c8d541cf7d60dd11d036047671caf1c502a3
kingo <kingo@DESKTOP-P0DINSI.lan>
2020-12-23 10:21:01 +0100

    Test CorsistaIdGenerator

---
+++ GestioneCorsi/src/test/it/betacom/milano/businesscomponent/idgenerator/CorsistaIdGeneratorTest.java
@@ -0,0 +1,29 @@
+package test.it.betacom.milano.businesscomponent.idgenerator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import it.betacom.milano.architetture.dao.DAOException;
+import it.betacom.milano.businnesscomponent.idgenerator.CorsistaIdGenerator;
+
+class CorsistaIdGeneratorTest {
+
+	@Test
+	void test() {
+		try {
+			CorsistaIdGenerator idGen = CorsistaIdGenerator.getInstace();
+			assertNotNull(idGen, "Istanza createa correttamente");
+			long valore = idGen.nextId();
+			assertEquals(valore, idGen.nextId() - 1, "Test sequenza");
+		} catch (DAOException | ClassNotFoundException | IOException exc) {
+			exc.printStackTrace();
+			fail("Fallito");
+		}
+	}
+
+}
